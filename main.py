c2a62af8b6d053e966dfd509443aae76c67f57fb
Matheus Archanjo <math.calazans56@gmail.com>
2017-04-17 15:59:29 -0300

    UPDATE SERVIDOR LOCAL E API TELEGRAM
    codigo python que adiciona as configurações locais e conecta o api do telegram com o codigo do Bot RODANDO NO SERVIDOR LOCAL `redis`

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+#RODANDO EM PYTHON 2.7,12
 import telegram
 import ConfigParser
 import requests
 import json
-#Setando BOT apartir de INI
+import redis
+from telegram.ext import Updater
 
+
+#Setando BOT apartir de INI
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
+
+#Pegando TOKEN BOT TELEGRAM
 TKB = config.get('DEFAULT','token')
+
+#UNINDO URL BOT + TOKEN
 url = 'https://api.telegram.org/bot' + TKB
+
+#CONECTANDO API TELGRAM
+#UPDATE RECEBE MENSAGEM
+#DISPATCHER RECEBE COMANDOS
+updater = Updater(token = TKB)
+dispatcher = updater.dispatcher
+
+#CONECTANDO AO SERVIDO REDIS EASY PEASY
+BD = redis.StrictRedis(host = config.get('DB','host'),
+					   port = config.getint('DB', 'port'),
+					   db = config.getint('DB', 'db'))
+
+
 getME = requests.get(url+'/getMe')
 if(getME.status_code == 200 ):
 	bot = json.loads(getME.content.decode('latin1'))
